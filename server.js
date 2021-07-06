cbeb80330c4d3abb8575a5abab4a500a18c2aaba
Adrien Sors <adrien.sors@gmail.com>
2021-07-06 15:53:26 +0200

    création de la route post et connexion à mongodb*

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
+// Connexion à la BdD
+fastify.register(require('fastify-mongodb'), {
+  // force to close the mongodb connection when app stopped
+  // the default value is false
+  forceClose: true,
+  // 27017 est le port de mongodb par défaut.
+  // pas besoin de login / password car localhost.
+  url: 'mongodb://localhost:27017/superheroes'
+})
+
 // Méthode API REST
 // GET →    READ
 // POST →   CREATE
@@ -9,36 +19,37 @@ const fastify = require('fastify')({ logger: true })
 // Declare a route
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
+  // return peut être remplacé par :
+  // reply.send(hello: 'world')
 });
 
 fastify.get('/me', function () {
     return {
         prenom: "Adrien",
         nom: "Nom",
-        job: "dev",
+        job: "dev"
     }
 });
 
-// Pas besoin de formulaire grace à l'extension "REST Client"
-fastify.get('/heroes', function() {
-  return {
-    name: "ironman"
-  }
-})
-
 const avengers = ["Ironman" , "Captain America", "Spiderman"];
 fastify.get('/heroes', function(){
-    return avengers;
+    return avengers
 })
 
-const student = "Siham";
+fastify.post('/heroes', (request, reply) => {
+  const collection = fastify.mongo.db.collection("heroes")
+  collection.insertOne(request.body)
+  return request.body
+})
+
+const student = "Siham"
 const age = 18;
 const data = {
     // cle: valeur
     // Si clé et valeur ont le même nom → forme raccourcie
     // student: student → student
     student,
-    age,
+    age
 }
 
 // Run the server!
